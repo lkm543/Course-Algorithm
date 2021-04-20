4c49b4547f0b687f98036aa0d399d2ee79dc3d7c
lkm543 <lkm543@gmail.com>
2021-04-20 09:45:04 +0800

    update

--- sidebar.php
+++ sidebar.php
@@ -9,20 +9,21 @@
     <li class='tab' data-tabid="Introduction"><a href="#Introduction">Introduction 課程簡介</a></li>
     <li class='tab' data-tabid="Chapter01"><a href="#Chapter01">Ch 01 演算法入門</a></li>
     <li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Ch 02 複雜度估算(Complexity)</a></li>
-    <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Ch 03 排序(Sort)</a></li>
+    <li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Ch 03 P 與 NP 問題</a></li>
+    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 排序(Sort)</a></li>
     <li class='tab' data-tabid="HW1"><a href="#HW1">Homework 1</a></li>
-    <li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Ch 04 搜尋(Search)</a></li>
-    <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Ch 05 分治法(Divide and Conquer)</a></li>
+    <li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Ch 05 搜尋(Search)</a></li>
+    <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Ch 06 分治法(Divide and Conquer)</a></li>
     <li class='tab' data-tabid="HW2"><a href="#HW2">Homework 2</a></li>
-    <li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Ch 06 貪婪演算法(Greedy Algorithm)</a></li>
-    <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Ch 07 動態規劃(Dynamic Programming)</a></li>
+    <li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Ch 07 貪婪演算法(Greedy Algorithm)</a></li>
+    <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Ch 08 動態規劃(Dynamic Programming)</a></li>
     <li class='tab' data-tabid="HW3"><a href="#HW3">Homework 3</a></li>
-    <li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Ch 08 圖論(Graph)</a></li>
-    <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Ch 09 廣度優先搜尋(BFS)</a></li>
-    <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Ch 10 深度優先搜尋(DFS)</a></li>
+    <li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Ch 09 圖論(Graph)</a></li>
+    <li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Ch 10 廣度優先搜尋(BFS)</a></li>
+    <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Ch 11 深度優先搜尋(DFS)</a></li>
     <li class='tab' data-tabid="HW4"><a href="#HW4">Homework 4</a></li>
-    <li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Ch 11 最小生成樹(MST)</a></li>
-    <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Ch 12 網路流(Flow Network)</a></li>
-    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 最短路徑</a></li>
+    <li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Ch 12 最小生成樹(MST)</a></li>
+    <li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Ch 13 網路流(Flow Network)</a></li>
+    <li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Ch 14 最短路徑</a></li>
     <li class='tab' data-tabid="HW5"><a href="#HW5">Homework 5</a></li>
 </ul>
